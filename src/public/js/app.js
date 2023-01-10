edc3ef5ee15b10375972b1fc28cac713bcaeedf0
jykim <jaeyeol.kim@lge.com>
2023-01-11 00:22:36 +0900

    3.9 complete, camera change not works

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -168,7 +168,19 @@ socket.on("ice", (ice) =>{
 
 // rtc codes
 function makeConnection(){
-    myPeerConnection = new RTCPeerConnection();
+    myPeerConnection = new RTCPeerConnection({
+        iceServers:[
+            {
+                urls: [
+                    "stun:stun.l.google.com:19302",
+                    "stun:stun1.l.google.com:19302",
+                    "stun:stun2.l.google.com:19302",
+                    "stun:stun3.l.google.com:19302",
+                    "stun:stun4.l.google.com:19302",
+                ],
+            },
+        ],
+    });
     myPeerConnection.addEventListener("icecandidate", hIce);
     myPeerConnection.addEventListener("addstream", hAddStream);
     myStream
